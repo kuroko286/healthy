f5f86fdf093c489c671accc549c82ffcfa52d800
Lê Văn Quốc <94890892+kuroko286@users.noreply.github.com>
2024-01-29 09:39:25 +0700

    containize with docker and add base websocket

--- src/main/java/com/kuroko/heathyapi/feature/DemoController.java
+++ src/main/java/com/kuroko/heathyapi/feature/DemoController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping
     public ResponseEntity<String> demo() {
-        return ResponseEntity.ok("Hello from secured endpoint");
+        return ResponseEntity.ok("Hello from secured endpoint change xcaASDAS");
     }
 }
---
+++ src/main/java/com/kuroko/heathyapi/feature/chatgpt/ChatResponse.java
@@ -0,0 +1,41 @@
+package com.kuroko.heathyapi.feature.chatgpt;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ChatResponse {
+    private List<Choice> choices;
+
+    // constructors, getters and setters
+
+    public static class Choice {
+
+        private int index;
+        private Message message;
+
+        public Choice(int index, Message message) {
+            this.index = index;
+            this.message = message;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public Message getMessage() {
+            return message;
+        }
+
+        public void setMessage(Message message) {
+            this.message = message;
+        }
+
+        // constructors, getters and setters
+    }
+}
